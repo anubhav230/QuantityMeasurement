8f7ebec9b95c6f264780f0f03f6ab6f5192d4376
anubhav230 <anubhavsingh230@gmail.com>
2020-07-13 18:46:35 +0530

    REFACTORED- Test case should throw nullpointer exception

--- src/main/java/com/quantitymeasurement/services/Feet.java
+++ src/main/java/com/quantitymeasurement/services/Feet.java
@@ -2,15 +2,17 @@ package com.quantitymeasurement.services;
 
 public class Feet {
     double value;
-    public Feet(double value) {
+
+    public Feet(Double value) {
         this.value = value;
     }
+
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-         Feet feet = (Feet) o;
+        Feet feet = (Feet) o;
         return Double.compare(feet.value, value) == 0;
     }
 
--- src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
@@ -1,6 +1,5 @@
 package com.quantitymeasurement;
 
-import com.quantitymeasurement.exception.QuantityMeasurementException;
 import com.quantitymeasurement.services.Feet;
 import com.quantitymeasurement.services.QuantityMeasurement;
 import org.junit.Assert;
@@ -12,19 +11,26 @@ public class QuantityMeasurementTest {
     @Test
     public void givenUnit_ShouldReturnConvertedValue() {
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-         double result = quantityMeasurement.covertUnit(2);
-        Assert.assertEquals(24, result,0.0);
+        double result = quantityMeasurement.covertUnit(2);
+        Assert.assertEquals(24, result, 0.0);
     }
+
 
     @Test
     public void givenSameUnit_ShouldReturnTrue() {
-        Feet feet1 = new Feet(0);
-        Feet feet2 = new Feet(0);
-        Assert.assertEquals(feet1,feet2);
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
     }
+
     @Test
-    public void givenSameUnit_ShouldReturnNotEqual() {
-               Feet feet1 = new Feet(0);
-               Assert.assertNotEquals(feet1, null);
+    public void givenSameUnit_ShouldReturnNotEqual() throws NullPointerException {
+        try {
+            Feet unit1 = new Feet(0.0);
+            Feet unit2 = new Feet(null);
+            Assert.assertEquals(unit1, unit2);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
